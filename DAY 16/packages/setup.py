56445da32847973b2d6bdfd95df5250a59dd4ef2
Dhruv Maisuria <dhruvmaisuriya123@gmail.com>
2024-03-20 15:48:20 +0530

    20/3/2024

---
+++ DAY 16/packages/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name="packageDhruv",
+      version="0.1",
+      description="This is code with Dhruv package",
+      long_description="This is a very very long descriptions",
+      author="Dhruv",
+      packages=['packageDhruv'],
+      install_requires=[])
